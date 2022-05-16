4e20fc0c1e0782018800d757f3d4649507968789
Giulio Pastorello <giulio.pastorello@gmail.com>
2022-05-16 17:48:55 +0200

    analisi ampiezza v_out, fit funzionante

---
+++ lab3/v_out_sweeps/3_bis/ampiezza_frequency_sweep_V_out_3_bis.C
@@ -0,0 +1,56 @@
+#include "TROOT.h"
+#include "TStyle.h"
+#include "TFile.h"
+#include "TH1F.h"
+#include "TF1.h"
+#include "TGraph.h"
+#include "TGraphErrors.h"
+#include "TLegend.h"
+#include "TCanvas.h"
+#include "TPad.h"
+#include <fstream>
+#include <iostream>
+
+double ampiezza (double* x, double* par)
+{
+double W = x[0] * (TMath::TwoPi());
+double wt2 = pow(W * par[0], 2.);
+double val = par[1] * abs((1 - wt2)/( sqrt(pow((1 - wt2), 2.) + 16*wt2))) + par[2];
+return val;
+}
+
+void analyse()
+{
+  F1 *f = new TF1("f", ampiezza, 1E3, 3*1E4, 3);
+  f->SetParameters(2.25 * 1E-5, 2.5, 0.06);
+
+  TGraph *graph1 = new TGraph("frequenza_V_out_1k-30k_3_bis.txt", "%lg %lg %*lg");
+  graph1->SetTitle("V_out frequency sweep 1k - 20k: ampiezza; frequenza(Hz); tensione(V)");
+  /*graph1->SetMarkerStyle(kOpenCircle);
+  graph1->SetMarkerSize(1);
+  graph1->SetMarkerColor(2);*/
+  graph1->SetLineColor(2);
+  graph1->SetLineWidth(4);
+  graph1->SetFillColor(0);
+
+  graph1->Fit("f", "R");
+  TF1 *fitFunc = graph1->GetFunction("f");
+
+  fitFunc->GetChisquare(); 
+  fitFunc->GetNDF(); 
+  fitFunc->GetParameter(0); 
+  fitFunc->GetParError(0);
+  fitFunc->GetParameter(1); 
+  fitFunc->GetParError(1);
+  fitFunc->GetParameter(2); 
+  fitFunc->GetParError(2);
+
+  std::cout << "ChiQuadro ridotto: " << fitFunc->GetChisquare() / fitFunc->GetNDF() << '\n';
+  std::cout << "Tau misurato: " << fitFunc->GetParameter(0) << " +/- " << fitFunc->GetParError(0) << '\n';
+  std::cout << "Ampiezza misurata: " << fitFunc->GetParameter(1) << " +/- " << fitFunc->GetParError(1) << '\n';
+  std::cout << "Intercetta misurata: " << fitFunc->GetParameter(2) << " +/- " << fitFunc->GetParError(2) << '\n';
+
+
+  TCanvas *c = new TCanvas("c");
+  graph1->Draw("AC");
+}
