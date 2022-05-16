26ca921dc19472f228d8608ef2cc1b3103e3fd49
Giulio Pastorello <giulio.pastorello@gmail.com>
2022-05-16 19:07:56 +0200

    fit fase, dà problemi singolarità

--- lab3/v_out_sweeps/3/fase_frequency_sweep_V_out_3.C
+++ lab3/v_out_sweeps/3/fase_frequency_sweep_V_out_3.C
@@ -8,17 +8,41 @@
 #include "TLegend.h"
 #include "TCanvas.h"
 #include "TPad.h"
+#include "TMath.h"
 #include <fstream>
 #include <iostream>
 #include <math.h>
 
+Double_t fase(Double_t *x, Double_t *par)
+{
+  Double_t w = x[0] * TMath::TwoPi();
+  Double_t wt = w * par[0];
+  double_t val = 180/TMath::Pi() * atan(4*wt/(pow(wt, 2.) - 1));
+  return val;
+}
+
 void analyse()
 {
+  TF1 *f = new TF1("f", fase, 1E3, 3*1E4, 1);
+  f->SetParameter(0, 2.25 * 1E-5);
+
   TGraph *graph1 = new TGraph("frequenza_V_out_1k-30k_3.txt", "%lg %*lg %lg");
   graph1->SetTitle("V_out frequency sweep 1k - 20k: fase; frequenza(Hz); tensione(V)");
   graph1->SetLineColor(6);
   graph1->SetLineWidth(4);
   graph1->SetFillColor(0);
+
+  graph1->Fit("f", "R");
+  TF1 *fitFunc = graph1->GetFunction("f");
+
+  fitFunc->GetChisquare(); 
+  fitFunc->GetNDF(); 
+  fitFunc->GetParameter(0); 
+  fitFunc->GetParError(0);
+
+  std::cout << "ChiQuadro ridotto: " << fitFunc->GetChisquare() / fitFunc->GetNDF() << '\n';
+  std::cout << "Tau misurato: " << fitFunc->GetParameter(0) << " +/- " << fitFunc->GetParError(0) << '\n';
+
 
   
   TCanvas *c = new TCanvas("c");
