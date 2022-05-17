bd9679c5be72e07d9fd7e003a58da10e85474a54
Giulio Pastorello <giulio.pastorello@gmail.com>
2022-05-17 10:44:04 +0200

    fit polinomiali funzionanti

--- lab3/passa_alto_sweep/ampiezza_frequency_sweep_PA.C
+++ lab3/passa_alto_sweep/ampiezza_frequency_sweep_PA.C
@@ -13,7 +13,7 @@
 #include <iostream>
 #include <math.h>
 
-Double_t pol5(Double_t *x, Double_t *par)
+/*Double_t pol5(Double_t *x, Double_t *par)
 {
   Double_t xx = x[0];
   Double_t val = par[0] + par[1] * xx + par[2] * pow(xx, 2.) +
@@ -21,22 +21,34 @@ Double_t pol5(Double_t *x, Double_t *par)
   return val; 
 }
 
+void plotFunc()
+{
+  TF1* func = new TF1("func", pol5, 6500, 8000);
+  func->SetParameters(58.5919, 89055.1, -49.1028, 0.0101373, -9.28748E-07, 3.18602E-11);
+  TCanvas c1 = new TCanvas("c1");
+  func->Draw();
+}*/
+
+
 void analyse()
 {
-  TF1 *f = new TF1("f", pol5, 6500, 8000, 6);
-  f->SetParameters(1, 1, 1, 1, 1, 1);
+  //TF1 *f = new TF1("f", pol5, 6500, 8000, 6);
+  //f->SetParameters(1, 1, 1, 1, 1, 1);
 
   TGraph *graph1 = new TGraph("frequenza_PA_sweep_1k-20k.txt", "%lg %lg %*lg");
   graph1->SetTitle("V_out frequency sweep 1k - 20k: ampiezza; frequenza(Hz); tensione(V)");
   /*graph1->SetMarkerStyle(kOpenCircle);
   graph1->SetMarkerSize(1);
   graph1->SetMarkerColor(2);*/
-  graph1->SetLineColor(2);
+  graph1->SetLineColor(5);
   graph1->SetLineWidth(4);
   graph1->SetFillColor(0);
 
-  graph1->Fit("f", "R");
-  TF1 *fitFunc = graph1->GetFunction("f");
+  graph1->Fit("pol5", "C", "SAME", 6500, 8000);
+  TF1 *fitFunc = graph1->GetFunction("pol5");
+
+  //graph1->Fit("f", "F", "R");
+  //TF1 *fitFunc = graph1->GetFunction("f");
 
   fitFunc->GetChisquare(); 
   fitFunc->GetNDF(); 
@@ -55,4 +67,5 @@ void analyse()
 
   TCanvas *c = new TCanvas("c");
   graph1->Draw("AC");
+  //fitFunc->Draw();
 }
--- lab3/passa_basso_sweep/ampiezza_frequency_sweep_PB.C
+++ lab3/passa_basso_sweep/ampiezza_frequency_sweep_PB.C
@@ -13,30 +13,38 @@
 #include <iostream>
 #include <math.h>
 
-Double_t pol5(Double_t *x, Double_t *par)
+/*Double_t pol5(Double_t *x, Double_t *par)
 {
   Double_t xx = x[0];
-  Double_t val = par[0] + par[1] * xx + par[2] * pow(xx, 2.) +
-                par[3] * pow(xx, 3.) + par[4] * pow(xx, 4.) + par[5] * pow(xx, 5.);
+  Double_t val = par[0] + par[1] * xx + par[2] * TMath::Power(xx, 2.) +
+                par[3] * TMath::Power(xx, 3.) + par[4] * TMath::Power(xx, 4.) + par[5] * TMath::Power(xx, 5.);
   return val; 
-}
+}*/
+
+/*void plotFunc()
+{
+  TF1 *func = new TF1("func", "[0] + [1] * x + [2] * TMath::Power(x, 2.) + [3] * TMath::Power(x, 3.) + [4] * TMath::Power(x, 4.) + [5] * TMath::Power(x,5.)", 6500, 8000);
+  func->SetParameters(-2.86073E16, 1.97458E13, -5.44523E09, 749913, -51.5774, 0.00141728);
+  TCanvas c1 = new TCanvas("c");
+  func->Draw();
+}*/
 
 void analyse()
 {
-  TF1 *f = new TF1("f", pol5, 6500, 8000, 6);
-  f->SetParameters(1, 1, 1, 1, 1, 1);
+  //TF1 *f = new TF1("f", "pol5");
+  //f->SetParameters(1, 1, 1, 1, 1, 1);
 
   TGraph *graph1 = new TGraph("frequenza_PB_sweep_1k-30k.txt", "%lg %lg %*lg");
   graph1->SetTitle("Passa Basso frequency sweep 1k - 30k: ampiezza; frequenza(Hz); ampiezza(V)");
   /*graph1->SetMarkerStyle(kOpenCircle);
   graph1->SetMarkerSize(1);
   graph1->SetMarkerColor(2);*/
-  graph1->SetLineColor(2);
+  graph1->SetLineColor(5);
   graph1->SetLineWidth(4);
   graph1->SetFillColor(0);
 
-  graph1->Fit("f", "R");
-  TF1 *fitFunc = graph1->GetFunction("f");
+  graph1->Fit("pol5", "C", "SAME", 6500, 8000);
+  TF1 *fitFunc = graph1->GetFunction("pol5");
 
   fitFunc->GetChisquare(); 
   fitFunc->GetNDF(); 
@@ -55,4 +63,5 @@ void analyse()
 
   TCanvas *c = new TCanvas("c");
   graph1->Draw("AC");
+  //fitFunc->Draw();
 }   
